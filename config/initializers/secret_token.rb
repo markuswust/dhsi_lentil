cae8e8c1f9394b142216aaa7377dc74e1bd0b4e1
Markus Wust <mgwust@ncsu.edu>
2015-05-21 13:37:18 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DhsiLentil::Application.config.secret_token = 'be523aa00b3883c5c252347a2cf509db01ef432b4265bdb50cd4de367a608f1f1ac66d04848510f1c4c435f0cebaba5b73fb8879cde4a09af69246160ab8577f'
